4fe893554165aa6e4002069061f70fc9768ba770
amamov <amamov@kakao.com>
2021-10-24 14:51:38 +0900

    create : project finish

--- project/app/main.py
+++ project/app/main.py
@@ -29,6 +29,17 @@ async def search(request: Request, q: str):
     keyword = q
     # (예외처리)
     #  - 검색어가 없다면 사용자에게 검색을 요구 return
+    if not keyword:
+        return templates.TemplateResponse(
+            "./index.html",
+            {"request": request, "title": "콜렉터 북북이"},
+        )
+    if await mongodb.engine.find_one(BookModel, BookModel.keyword == keyword):
+        books = await mongodb.engine.find(BookModel, BookModel.keyword == keyword)
+        return templates.TemplateResponse(
+            "./index.html",
+            {"request": request, "title": "콜렉터 북북이", "books": books},
+        )
     #  - 해당 검색어에 대해 수집된 데이터가 이미 DB에 존재한다면 해당 데이터를 사용자에게 보여준다. return
     # 2. 데이터 수집기로 해당 검색어에 대해 데이터를 수집한다.
     naver_book_scraper = NaverBookScraper()
@@ -49,7 +60,7 @@ async def search(request: Request, q: str):
     #  - 각 모델 인스턴스를 DB에 저장한다.
     return templates.TemplateResponse(
         "./index.html",
-        {"request": request, "title": "콜렉터 북북이"},
+        {"request": request, "title": "콜렉터 북북이", "books": books},
     )
 
 
